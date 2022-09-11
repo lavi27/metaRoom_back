65de5c5022ee7a6f812137f3ac2da7d2f51f4d1f
lavi27 <npt1237@gmail.com>
2022-09-11 22:23:27 +0900

    chore : IsNotEmpty 붙임

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,15 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class createUserDto {
     @IsString()
+    @IsNotEmpty()
     readonly name: string;
 
     @IsString()
+    @IsNotEmpty()
     readonly pw: string;
 
-    @IsString({ each: true })
-    readonly asdf: string[];
+    // @IsString({ each: true })
+    // @IsNotEmpty()
+    // readonly asdf: string[];
 }
