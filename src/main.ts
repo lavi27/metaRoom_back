681a04f1689eeb207f66fb8c959e68aa3903b1a5
lavi27 <npt1237@gmail.com>
2022-09-11 22:22:19 +0900

    fix : cors 에러 해결

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://127.0.0.1:5173',
+    credentials: true,
+  });
 
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true,
